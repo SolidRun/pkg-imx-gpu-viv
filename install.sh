27e950561067efac72f98a8f3a3d53fe0b1e9f47
Josua Mayer <josua@solid-run.com>
2015-06-13 23:39:54 +0800

    move everything into /usr/lib/galcore to no longer collide with mesa

--- install.sh
+++ install.sh
@@ -7,88 +7,86 @@ source functions.inc
 # Vivante core graphics libraries
 install_core_base() {
 	# Vivante GPU HAL
-	for bg in fb dfb wl; do
-		install_lib libGAL-$bg.so imx-gpu-viv-$bg/libGAL.so
-		install_lib libGAL_egl.$bg.so imx-gpu-viv-$bg/libGAL_egl.so
+	for bg in fb dfb wl x11; do
+		install_gc_lib libGAL-$bg.so
 	done
-	install_lib libGAL-x11.so imx-gpu-viv-x11/libGAL.so
-	install_lib libGAL_egl.dri.so imx-gpu-viv-x11/libGAL_egl.so
-	link_lib imx-gpu-viv/libGAL.so libGAL.so
-	link_lib imx-gpu-viv/libGAL_egl.so libGAL_egl.so
+	# alternatives: libGAL.so -> libGAL-$bg.so
+
+	for bg in fb dfb wl; do
+		install_gc_lib libGAL_egl.$bg.so
+	done
+	install_gc_lib libGAL_egl.dri.so libGAL_egl.x11.so
+	# alternatives: libGAL_egl.so -> libGAL_egl.$bg.so
 	install_headers HAL
 
 	# Khronos shared headers
 	install_headers KHR
 
 	# EGL
-	link_lib libEGL.so.1.0 libEGL.so.1.0.0 # compatibility symlink to the Mesa soname of libEGL
+	link_gc_lib libEGL.so.1.0 libEGL.so.1.0.0 # compatibility symlink to the Mesa soname of libEGL
 	for bg in fb dfb wl x11; do
-		install_lib libEGL-$bg.so imx-gpu-viv-$bg/libEGL.so.1.0
+		install_gc_lib libEGL-$bg.so
 	done
-	link_lib imx-gpu-viv/libEGL.so.1.0 libEGL.so.1.0
-	install_custom_pc egl_fb ../imx-gpu-viv-fb/egl
-	install_custom_pc egl_dfb ../imx-gpu-viv-dfb/egl
-	install_custom_pc egl_wl ../imx-gpu-viv-wl/egl
-	install_custom_pc egl_x11 ../imx-gpu-viv-x11/egl
-	link_lib ../imx-gpu-viv/egl.pc pkgconfig/egl.pc # make more readable
-	link_lib libEGL.so.1.0 libEGL.so.1
-	link_lib libEGL.so.1 libEGL.so
+	# alternatives: libEGL.so.1.0 -> libEGL-$bg.so
+	install_custom_gc_pc egl
+	link_gc_lib libEGL.so.1.0 libEGL.so.1
+	link_gc_lib libEGL.so.1 libEGL.so
 	install_headers EGL
 
 	# OpenGL-ES
-	install_lib libGLES_CL.so.1.1.0
-	link_lib libGLES_CL.so.1.1.0 libGLES_CL.so.1
-	link_lib libGLES_CL.so.1 libGLES_CL.so
-	install_lib libGLES_CM.so.1.1.0
-	link_lib libGLES_CM.so.1.1.0 libGLES_CM.so.1
-	link_lib libGLES_CM.so.1 libGLES_CM.so
+	install_gc_lib libGLES_CL.so.1.1.0
+	link_gc_lib libGLES_CL.so.1.1.0 libGLES_CL.so.1
+	link_gc_lib libGLES_CL.so.1 libGLES_CL.so
+	install_gc_lib libGLES_CM.so.1.1.0
+	link_gc_lib libGLES_CM.so.1.1.0 libGLES_CM.so.1
+	link_gc_lib libGLES_CM.so.1 libGLES_CM.so
 	install_headers GLES
 
 	# OpenGL-ES 1.1
-	install_lib libGLESv1_CL.so.1.1.0
-	link_lib libGLESv1_CL.so.1.1.0 libGLESv1_CL.so.1
-	link_lib libGLESv1_CL.so.1 libGLESv1_CL.so
-	install_lib libGLESv1_CM.so.1.1.0
-	link_lib libGLESv1_CM.so.1.1.0 libGLESv1_CM.so.1
-	link_lib libGLESv1_CM.so.1 libGLESv1_CM.so
-	install_custom_pc glesv1_cm
+	install_gc_lib libGLESv1_CL.so.1.1.0
+	link_gc_lib libGLESv1_CL.so.1.1.0 libGLESv1_CL.so.1
+	link_gc_lib libGLESv1_CL.so.1 libGLESv1_CL.so
+	install_gc_lib libGLESv1_CM.so.1.1.0
+	link_gc_lib libGLESv1_CM.so.1.1.0 libGLESv1_CM.so.1
+	link_gc_lib libGLESv1_CM.so.1 libGLESv1_CM.so
+	install_custom_gc_pc glesv1_cm
 
 	# OpenGL-ES 2.0
 	for bg in fb dfb wl x11; do
-		install_lib libGLESv2-$bg.so imx-gpu-viv-$bg/libGLESv2.so.2.0.0
+		install_gc_lib libGLESv2-$bg.so
 	done
-	link_lib imx-gpu-viv/libGLESv2.so.2.0.0 libGLESv2.so.2.0.0
-	link_lib libGLESv2.so.2.0.0 libGLESv2.so.2
-	link_lib libGLESv2.so.2 libGLESv2.so
+	link_gc_lib libGLESv2.so.2 libGLESv2.so.2.0.0 # Mesa compat
+	# alternatives: libGLESv2.so.2 -> libGLESv2-$bg.so
+	link_gc_lib libGLESv2.so.2 libGLESv2.so
 	install_headers GLES2
-	install_custom_pc glesv2
+	install_custom_gc_pc glesv2
 
 	# OpenGL-ES 3.0
 	# part of libGLESv2.so
 	install_headers GLES3
 
 	# GL Shader Compiler
-	install_lib libGLSLC.so
+	install_gc_lib libGLSLC.so
 
 	# OpenVG
-	install_lib libOpenVG.2d.so
-	install_lib libOpenVG.3d.so
-	link_lib libOpenVG.3d.so libOpenVG.so
+	install_gc_lib libOpenVG.2d.so
+	install_gc_lib libOpenVG.3d.so
+	link_gc_lib libOpenVG.3d.so libOpenVG.so
 	install_headers VG
-	install_custom_pc vg
+	install_custom_gc_pc vg
 	# TODO: alternative for VG?
 
 	if [ "x$skip_cl" = "xno" ]; then
 	# OpenCL (only GC2000)
-		install_lib libOpenCL.so
-		install_lib libCLC.so
+		install_gc_lib libOpenCL.so
+		install_gc_lib libCLC.so
 		install_headers CL
 
-		install_lib libVivanteOpenCL.so
+		install_gc_lib libVivanteOpenCL.so
 	fi
 
 	# VDK
-	install_lib libVDK.so
+	install_gc_lib libVDK.so
 	install_header gc_vdk.h
 	install_header gc_vdk_hal.h
 	install_header gc_vdk_types.h
@@ -96,11 +94,11 @@ install_core_base() {
 
 	# miscellaneous
 	install_conf Vivante.icd
-	install_lib libVSC.so
+	install_gc_lib libVSC.so
 	for bg in fb dfb wl x11; do
-		install_lib libVIVANTE-$bg.so imx-gpu-viv-$bg/libVIVANTE.so
+		install_gc_lib libVIVANTE-$bg.so
 	done
-	link_lib imx-gpu-viv/libVIVANTE.so libVIVANTE.so
+	# alternatives: libVIVANTE.so -> libVIVANTE-$bg.so
 
 	return
 }
@@ -117,10 +115,10 @@ install_core_dfb() {
 
 install_core_x11() {
 	# X11 OpenGL GLX
-	install_lib libGL.so.1.2
-	link_lib libGL.so.1.2 libGL.so.1
-	link_lib libGL.so.1 libGL.so
-	install_custom_pc gl
+	install_gc_lib libGL.so.1.2
+	link_gc_lib libGL.so.1.2 libGL.so.1
+	link_gc_lib libGL.so.1 libGL.so
+	install_custom_gc_pc gl
 
 	# DRI
 	install_dri_driver vivante_dri.so
@@ -128,19 +126,19 @@ install_core_x11() {
 }
 
 install_core_wl() {
-	install_custom_pc wayland-egl
+	install_custom_gc_pc wayland-egl
 
 	# Wayland libs
-	install_lib libgc_wayland_protocol.so.0.0.0
-	link_lib libgc_wayland_protocol.so.0.0.0 libgc_wayland_protocol.so.0
-	link_lib libgc_wayland_protocol.so.0 libgc_wayland_protocol.so
-	install_lib libgc_wayland_protocol.a
+	install_gc_lib libgc_wayland_protocol.so.0.0.0
+	link_gc_lib libgc_wayland_protocol.so.0.0.0 libgc_wayland_protocol.so.0
+	link_gc_lib libgc_wayland_protocol.so.0 libgc_wayland_protocol.so
+	install_gc_lib libgc_wayland_protocol.a
 	install_pc gc_wayland_protocol
 
-	install_lib libwayland-viv.so.0.0.0
-	link_lib libwayland-viv.so.0.0.0 libwayland-viv.so.0
-	link_lib libwayland-viv.so.0 libwayland-viv.so
-	install_lib libwayland-viv.a
+	install_gc_lib libwayland-viv.so.0.0.0
+	link_gc_lib libwayland-viv.so.0.0.0 libwayland-viv.so.0
+	link_gc_lib libwayland-viv.so.0 libwayland-viv.so
+	install_gc_lib libwayland-viv.a
 	install_pc wayland-viv
 
 	# TODO: update paths in installed .pc-files
@@ -148,9 +146,13 @@ install_core_wl() {
 }
 
 install_core_alternatives() {
-	update-alternatives --remove-all vivante
+	update-alternatives --remove-all vivante-gal
 	for bg in fb dfb wl x11; do
-		update-alternatives --install /usr/lib/imx-gpu-viv vivante /usr/lib/imx-gpu-viv-$bg 10
+		update-alternatives \
+			--install /usr/lib/galcore/libGAL.so vivante-gal /usr/lib/galcore/libGAL-$bg.so 10 \
+			--slave /usr/lib/galcore/libEGL.so.1.0 vivante-egl /usr/lib/galcore/libEGL-$bg.so \
+			--slave /usr/lib/galcore/libGLESv2.so.2 vivante-gles2 /usr/lib/galcore/libGLESv2-$bg.so \
+			--slave /usr/lib/galcore/libVIVANTE.so vivante-vivante /usr/lib/galcore/libVIVANTE-$bg.so
 	done
 }
 
@@ -188,7 +190,7 @@ install_tools() {
 install_apitrace_nonx11() {
 	install_bin apitrace
 	install_bin eglretrace
-	install_lib apitrace/wrappers/egltrace.so
+	install_gc_lib apitrace/wrappers/egltrace.so
 	mkdir -p "${_destdir}/usr/lib/apitrace"
 	cp -rv "${SOURCESDIR}/usr/lib/apitrace/scripts" "${_destdir}/${_libdir}/apitrace/"
 	mkdir -p "${_destdir}/usr/share/doc/"
@@ -199,7 +201,7 @@ install_apitrace_nonx11() {
 install_apitrace_x11() {
 	install_apitrace_nonx11
 	install_bin glretrace
-	install_lib apitrace/wrappers/glxtrace.so
+	install_gc_lib apitrace/wrappers/glxtrace.so
 	return
 }
 
